0b82db42817f068666f0814e52f86232a67602a6
Krishna Tikariya <krishnatikariya16@gmail.com>
2024-06-03 12:12:08 +0530

    mongodb install and connectd

--- backend/config.js
+++ backend/config.js
@@ -1,1 +1,2 @@
 export const PORT = 5555;  
+export const mongoDBURL = 'mongodb+srv://root:root@book-store.jpfmrx4.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Book-Store'
--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,19 @@
 import express from "express";
+import { PORT , mongoDBURL} from "./config.js";
+import mongoose from 'mongoose';
 
 const app = express();
+
+app.get('/', (request,response) =>{
+    console.log(request)
+    return response.status(234).send("hello")
+});
+app.listen(PORT ,() =>{
+    console.log(`App is listen to port: ${PORT}`);
+});
+
+mongoose.connect(mongoDBURL).then(()=>{
+    console.log('App connected to database');
+}).catch((error)=>{
+    console.error(error);
+});
