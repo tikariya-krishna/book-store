b6ef8867337b02166763d00b767ca887f67be996
Krishna Tikariya <krishnatikariya16@gmail.com>
2024-06-17 19:36:26 +0530

    add schema

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { request, response } from "express";
 import { PORT , mongoDBURL} from "./config.js";
 import mongoose from 'mongoose';
 
@@ -8,12 +8,39 @@ app.get('/', (request,response) =>{
     console.log(request)
     return response.status(234).send("hello")
 });
-app.listen(PORT ,() =>{
-    console.log(`App is listen to port: ${PORT}`);
+
+app.post('/books', async(request,response)=>{
+    try {
+        if (
+            !request.body.title||
+            !request.body.author||
+            !request.body.publishYear
+        ){
+            return response.status(400).send({
+                message:'send all required fields: title,author,publishYear',
+
+            });
+        }
+        const newBook = {
+            title: request.body.title,
+            author: request.body.author,
+            publishYear: request.body.publishYear,
+        };
+        const book = await Book.create(newBook);
+
+        return response.status(201).send(book);
+    } catch (error) {
+        console.log(error.message);
+        response.status(500).send({message: error.message});
+    }
 });
 
 mongoose.connect(mongoDBURL).then(()=>{
     console.log('App connected to database');
+    app.listen(PORT ,() =>{
+        console.log(`App is listen to port: ${PORT}`);
+    });
+
 }).catch((error)=>{
     console.error(error);
 });
