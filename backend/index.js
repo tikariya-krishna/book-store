ea1f26dae6553b3656e57045c7c7a611de8c9be3
Krishna Tikariya <krishnatikariya16@gmail.com>
2024-06-20 10:49:15 +0530

    Update Books Detail

--- backend/index.js
+++ backend/index.js
@@ -53,6 +53,44 @@ app.get('/books' , async(request,response)=>{
 
 });
 
+app.get('/books/:id' , async(request,response)=>{
+    try {
+        const { id } = request.params;
+        const books = await Book.findById(id);
+        return response.status(200).json(books);
+    } catch (error) { 
+        console.log(error.message);
+        response.status(500).send({message: error.message});
+    }
+});
+
+
+app.put('/books/:id', async(request,response)=>{
+    try {
+        if (
+            !request.body.title||
+            !request.body.author||
+            !request.body.publisherYear
+        ){
+            return response.status(400).send({
+                message:'send all required fields: title,author,publishYear',
+
+            });
+        }
+        const {id} = request.params;
+        const result = await Book.findByIdAndUpdate(id,request.body); 
+
+        if(!result){
+            return response.status(404).json({message:'Book in not found'});
+        }
+            return response.status(200).send({message:'Book is updated'});
+
+    } catch (error) {
+        console.log(error.message);
+        response.status(500).send({message: error.message});
+    }
+});
+
 mongoose.connect(mongoDBURL).then   (()=>{
     console.log('App connected to database');
     app.listen(PORT ,() =>{
